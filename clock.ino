85300191102bc16b5285eab706abd7874e35c0a7
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-05 15:48:57 +0530

    Add files via upload

---
+++ clock.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64  
+#define OLED_RESET    -1  
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+
+
+unsigned long previousMillis = 0;
+const long interval = 1000; 
+
+void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  
+  display.clearDisplay();
+  display.display();
+
+  Serial.begin(9600);
+}
+
+void loop() {
+ 
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    incrementTime();
+  }
+  
+  displayTime();
+}
+
+void incrementTime() {
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+      if (hours >= 24) {
+        hours = 0;
+      }
+    }
+  }
+}
+
+void displayTime() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+
+  char timeString[9];
+  sprintf(timeString, "%02d:%02d:%02d", hours, minutes, seconds);
+  
+  display.setCursor(0, 20);
+  display.print(timeString);
+  
+  display.display();
+}
+
